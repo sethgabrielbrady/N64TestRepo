1ee295aa1650c6f440993a8661494fc402a22844
sethgabrielbrady <sethgabrielbrady@gmail.com>
2024-12-03 12:47:15 -0500

    Adds sprite and plane with controls

--- code/test_plane/main.c
+++ code/test_plane/main.c
@@ -1,3 +1,5 @@
+
+// we may want to use a plane like the font_billboard in snake3d
 // use rdpq_sprite_blit to draw the sprite -- see tiny3d/examples/99_testscene/main.c
 
 #include <libdragon.h>
@@ -6,8 +8,6 @@
 #include <t3d/t3dmodel.h>
 #include <t3d/t3ddebug.h>
 #include <stdlib.h>
-
-
 
 
 surface_t *depthBuffer;
@@ -19,17 +19,69 @@ T3DVec3 camPos;
 T3DVec3 camTarget;
 T3DVec3 lightDirVec;
 rspq_syncpoint_t syncPoint;
+sprite_t* fulgore;
+float posX = 0.0f;
+float posY = 0.0f;
+joypad_inputs_t joypad;
+
+// sprite_t* fulgore = sprite_load("rom:/fulgoresing.sprite");
+
+typedef struct rdpq_blitparms_s {
+	rdpq_tile_t tile;
+	int s0;
+	int t0;
+	int width;
+	int height;
+	bool flip_x;
+	bool flip_y;
+
+	int cx;
+	int cy;
+	float scale_x;
+	float scale_y;
+	float theta;
+
+	bool filtering;
+	int nx;
+	int ny;
+} rdpq_blitparms_s;
+
+rdpq_blitparms_s params = {
+  // .tile = RDPQ_TILE_0,
+  .s0 = 0,
+  .t0 = 0,
+  .width = 0,
+  .height = 0,
+  .flip_x = false,
+  .flip_y = false,
+  .cx = 0,
+  .cy = 0,
+  .scale_x = 1.0f,
+  .scale_y = 1.0f,
+  .theta = 0.0f,
+  .filtering = false,
+  .nx = 0,
+  .ny = 0,
+};
+
+
+
+
 
 void game_init(void)
 {
   debug_init_isviewer();
   debug_init_usblog();
   joypad_init();
+  joypad = joypad_get_inputs(0);
+
   asset_init_compression(2);
   dfs_init(DFS_DEFAULT_LOCATION);
   rdpq_init();
   display_init(RESOLUTION_320x240, DEPTH_16_BPP, 3, GAMMA_NONE, FILTERS_RESAMPLE_ANTIALIAS);
   depthBuffer = display_get_zbuf();
+
+
 
   t3d_init((T3DInitParams){});
 
@@ -44,6 +96,8 @@ void game_init(void)
   lightDirVec = (T3DVec3){{1.0f, 1.0f, 1.0f}};
   t3d_vec3_norm(&lightDirVec);
 
+  fulgore = sprite_load("rom:/fulgoresing.sprite");
+
   modelMap = t3d_model_load("rom:/map.t3dm");
 
   rspq_block_begin();
@@ -56,6 +110,8 @@ void game_init(void)
 }
 
 
+
+
 void game_loop(float deltaTime)
 {
   uint8_t colorAmbient[4] = {0xAA, 0xAA, 0xAA, 0xFF};
@@ -63,6 +119,8 @@ void game_loop(float deltaTime)
 
   t3d_viewport_set_projection(&viewport, T3D_DEG_TO_RAD(90.0f), 20.0f, 160.0f);
   t3d_viewport_look_at(&viewport, &camPos, &camTarget, &(T3DVec3){{0,1,0}});
+
+
 
 
   // ======== Draw (3D) ======== //
@@ -82,9 +140,22 @@ void game_loop(float deltaTime)
   rspq_block_run(dplMap);
   syncPoint = rspq_syncpoint_new();
 
+
+     // add sprite
+  // rdpq_set_mode_copy(true);
+  rdpq_set_mode_standard();
+  rdpq_mode_combiner(RDPQ_COMBINER_TEX);
+  rdpq_mode_blender(RDPQ_BLENDER_MULTIPLY);
+  joypad_inputs_t joypad = joypad_get_inputs(0);
+  posX += (float)joypad.stick_x * -0.05f;
+  posY += (float)joypad.stick_y * -0.05f;
+  params.scale_x = params.scale_y = 0.9f;
+  rdpq_sprite_blit(fulgore, posX, posY, &params);
+  //rspq_wait();
   rdpq_sync_tile();
   rdpq_sync_pipe(); // Hardware crashes otherwise
   rdpq_detach_show();
+  joypad_poll();
 }
 
 void game_cleanup(void)
@@ -98,12 +169,13 @@ void game_cleanup(void)
 
 int main()
 {
+
   game_init();
 
   while(1)
   {
-    joypad_poll();
     game_loop(0.0f);
+
   }
 
   game_cleanup();
