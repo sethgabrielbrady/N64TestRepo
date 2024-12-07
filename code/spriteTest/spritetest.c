aef2d9f0d25bb34995f892028c89387a7643e9c4
sethgabrielbrady <sethgabrielbrady@gmail.com>
2024-12-07 15:12:59 -0500

    Updates with sprite sheet

--- code/spriteTest/spritetest.c
+++ code/spriteTest/spritetest.c
@@ -7,14 +7,24 @@
 // #include <stdlib.h>
 
 //Animation frame size defines
-#define ANIM_FRAME_W 100
-#define ANIM_FRAME_H 130
+#define ANIM_FRAME_W 110
+#define ANIM_FRAME_H 140
+// #define ANIM_FRAME_W 100
+// #define ANIM_FRAME_H 130
 
 //Animation frame timing defines
-#define ANIM_FRAME_DELAY 5
-#define ANIM_FRAME_MAX 10
+#define ANIM_FRAME_DELAY 3.5
+#define ANIM_FRAME_MAX 15
+#define ANIM_COL_MAX 5
+#define ANIM_ROW_MAX 3
 
 #define KNIGHT_MAX 2
+
+int heightCounter = 0;
+int currentFrame = 0;
+char str[2];
+int frame;
+
 
 //Structure for knight sprite
 typedef struct {
@@ -23,12 +33,14 @@ typedef struct {
     bool attack;
     bool flip;
     int time;
+
 } knight_data;
 
 static knight_data knights[2];
 static sprite_t *sheet_knight;
 static sprite_t *sheet_knight2;
 static sprite_t *current_knight;
+display_context_t disp;
 
 void render(void)
 {
@@ -38,6 +50,8 @@ void render(void)
     //Set render mode to the standard render mode
     rdpq_set_mode_standard();
     rdpq_mode_alphacompare(1); // colorkey (draw pixel with alpha >= 1)
+
+
     for(int i=0; i<KNIGHT_MAX; i++) {
 
         if (i>0) {
@@ -45,15 +59,24 @@ void render(void)
         } else {
             current_knight = sheet_knight;
         }
-        int frame = knights[i].time/ANIM_FRAME_DELAY; //Calculate knight frame
+
+        if (heightCounter == ANIM_ROW_MAX) {
+            heightCounter = 0;
+        }
+        frame = knights[i].time/ANIM_FRAME_DELAY; //Calculate knight frame
         //Draw knight sprite
+
         rdpq_sprite_blit(current_knight, knights[i].x, knights[i].y, &(rdpq_blitparms_t){
-            .s0 = frame*ANIM_FRAME_W, //Extract correct sprite from sheet
+            .s0 = 110*frame,
+            .t0 = 140*heightCounter,
+            // .s0 = frame*ANIM_FRAME_W, //Extract correct sprite from sheet
+            // .t0 = 0,
             //Set sprite center to bottom-center
             .cx = ANIM_FRAME_W/2,
             .cy = ANIM_FRAME_H,
             .width = ANIM_FRAME_W, //Extract correct width from sheet
-            .flip_x = knights[i].flip
+            .height = ANIM_FRAME_H,
+            .flip_x = knights[i].flip,
         });
 
     }
@@ -64,12 +87,28 @@ void render(void)
 void update(void)
 {
     for(int i=0; i<KNIGHT_MAX; i++) {
-        if(knights[i].attack) {
+        if(knights[i].attack ) {
             knights[i].time++; //Increment time
             //Stop attack at end of animation
-            if(knights[i].time >= ANIM_FRAME_DELAY*ANIM_FRAME_MAX) {
+
+
+        //  if(knights[i].time >= ANIM_FRAME_DELAY*ANIM_FRAME_MAX) {
+
+            // if  (heightCounter == ANIM_ROW_MAX ) {
+            //     knights[i].time = 0;
+            //     heightCounter = 0;
+            //     knights[i].attack = false;
+            // }
+
+            if (knights[i].time >= ANIM_FRAME_DELAY*ANIM_COL_MAX) {
+                heightCounter += 1;
                 knights[i].time = 0;
-                // knights[i].attack = false;
+
+                if(knights[i].time >= ANIM_FRAME_DELAY*ANIM_FRAME_MAX) {
+                    knights[i].time = 0;
+                    heightCounter = 0;
+                    knights[i].attack = false;
+                }
             }
         }
     }
@@ -90,8 +129,8 @@ int main()
     //Init joypad
     joypad_init();
     //Load Sprite Sheet
-    sheet_knight = sprite_load("rom:/fulgorestandhq.sprite");
-    sheet_knight2 = sprite_load("rom:/fulgorestandlq.sprite");
+    sheet_knight = sprite_load("rom:/fulgorewalkhq.sprite");
+    sheet_knight2 = sprite_load("rom:/fulgorewalklq.sprite");
     //Initialize left fulgore
     knights[0].x = (display_get_width()/2)-50;
     knights[0].y = display_get_height()-15;
@@ -99,15 +138,28 @@ int main()
     knights[1].x = (display_get_width()/2)+50;
     knights[1].y = knights[0].y;
     knights[1].flip = true;
+
+
+
+    //display_context_t disp = 0;
     while (1)
     {
         render();
         update();
+        // while (!(disp = display_lock()));
+        // graphics_fill_screen(disp, graphics_make_color(0, 0, 0, 0));
+        // sprintf( str, "%d", heightCounter);
+
+        // graphics_draw_text(disp, 10, 10, str );
+        // display_show(disp);
+
+
 
         //Read joypad
         joypad_poll();
         joypad_buttons_t ckeys = joypad_get_buttons_pressed(JOYPAD_PORT_1);
         //Set attack for left knight
+        // Draw text on the screen
         if(ckeys.a) {
             knights[0].attack = !knights[0].attack;
         }
@@ -117,3 +169,5 @@ int main()
         }
     }
 }
+
+
