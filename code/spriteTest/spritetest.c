d870e1893d41063b5461f61906b0a4360c95ae18
sethgabrielbrady <sethgabrielbrady@gmail.com>
2024-12-08 23:56:41 -0500

    update

--- code/spriteTest/spritetest.c
+++ code/spriteTest/spritetest.c
@@ -7,30 +7,25 @@
 // #include <stdlib.h>
 
 
-//combine the animations into 1 sprite sheet
 // look at putting the sprite on a triangle or a plane
+// start incorporating state machines
 
 //Animation frame size defines
 #define ANIM_FRAME_W 110
 #define ANIM_FRAME_H 140
+#define ANIM_FRAME_DELAY 4.5
+
 
 #define ANIM_FULG_STAND_FRAME_MAX 10
 #define ANIM_FULG_STAND_ROW_MAX 5
-
-
-
-
-//Animation frame timing defines
-#define ANIM_FRAME_DELAY 4.5
 #define ANIM_FULG_WALK_FRAME_MAX 15
-#define ANIM_FULG_WALK_COL_MAX 5
 #define ANIM_FULG_WALK_ROW_MAX 5
 
-
-int tile_sheet_row_index = 0;
+int current_sheet_row_index = 0;
 int walk_start_index = 2;
+int standing_start_index = 0;
+
 int frame;
-int sheet_index_start = 0;
 char str[32];
 
 //Structure for fighter sprite
@@ -57,25 +52,25 @@ void render(void)
     rdpq_mode_alphacompare(1); // colorkey (draw pixel with alpha >= 1)
 
 
-    if(fighter.walk_forward )
+    if (fighter.walk_forward )
+
     {
-        if (tile_sheet_row_index == ANIM_FULG_WALK_ROW_MAX) {
-            tile_sheet_row_index = walk_start_index;
-            sheet_index_start = ANIM_FRAME_H*tile_sheet_row_index;
+        if (current_sheet_row_index == ANIM_FULG_WALK_ROW_MAX)
+        {
+            current_sheet_row_index = walk_start_index;
         }
     }
     else
     {
-        sheet_index_start = 0;
-        tile_sheet_row_index = 0;
+        current_sheet_row_index = standing_start_index;
     }
 
-    frame = 0;
+    // frame = 0;
     frame = fighter.time/ANIM_FRAME_DELAY; //Calculate fighter frame
     //Draw fighter sprite
     rdpq_sprite_blit(fighter_1, fighter.x, fighter.y, &(rdpq_blitparms_t){
         .s0 = frame*ANIM_FRAME_W, //Extract correct sprite from sheet
-        .t0 = sheet_index_start,
+        .t0 = current_sheet_row_index*ANIM_FRAME_H,
         // .t0 = 0,
         //Set sprite center to bottom-center
         .cx = ANIM_FRAME_W/2,
@@ -90,30 +85,29 @@ void render(void)
 
 void update(void)
 {
-    if(fighter.walk_forward ) {
+    fighter.time++; //Increment time
 
-        fighter.time++; //Increment time
+    if(fighter.walk_forward ) {
         //Stop walk at end of animation
 
-        if (fighter.time >= ANIM_FRAME_DELAY*ANIM_FULG_WALK_COL_MAX) {
-            tile_sheet_row_index += 1;
+        if (fighter.time >= ANIM_FRAME_DELAY*ANIM_FULG_WALK_ROW_MAX) {
+            current_sheet_row_index += 1;
             fighter.time = 0;
 
             if(fighter.time >= ANIM_FRAME_DELAY*ANIM_FULG_WALK_FRAME_MAX) {
                 fighter.time = 0;
-                tile_sheet_row_index = 2;
+                current_sheet_row_index = standing_start_index;
                 fighter.walk_forward = false;
             }
         }
     } else {
-        tile_sheet_row_index = 0;
-        fighter.time++;
-
+        // if the time is greater than the max time for first 5 cells of the standing animation
          if (fighter.time >= ANIM_FRAME_DELAY*ANIM_FULG_STAND_ROW_MAX) {
-            tile_sheet_row_index += 1;
+            current_sheet_row_index += 1;
+            fighter.time = 0;
         } else if (fighter.time >= ANIM_FRAME_DELAY*ANIM_FULG_STAND_FRAME_MAX)
         {
-            tile_sheet_row_index = 0;
+            current_sheet_row_index = standing_start_index;
             fighter.time = 0;
         }
     }
@@ -148,7 +142,7 @@ int main()
         // display_context_t disp = 0;
         // while (!(disp = display_lock()));
         // graphics_fill_screen(disp, graphics_make_color(0, 0, 0, 0));
-        // sprintf( str, "%d", tile_sheet_row_index);
+        // sprintf( str, "%d", current_sheet_row_index);
         // graphics_draw_text(disp, 10, 10, str );
         // display_show(disp);
 
@@ -160,10 +154,11 @@ int main()
         // Draw text on the screen
         if(btnHeld.d_right) {
             fighter.walk_forward = true;
-        }else {
-            fighter.walk_forward = false;
-            tile_sheet_row_index = 0;
         }
+        // else {
+        //     fighter.walk_forward = false;
+        //     // current_sheet_row_index = 0;
+        // }
 
         // //Set attack for right fighter
         // if(ckeys.b) {
