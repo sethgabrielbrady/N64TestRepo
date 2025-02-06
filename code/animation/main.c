753f4cc78009d55c4999980c29b950a5f25ba540
sethgabrielbrady <sethgabrielbrady@gmail.com>
2025-02-06 10:57:13 -0500

    Updates height map

--- code/animation/main.c
+++ code/animation/main.c
@@ -21,6 +21,15 @@ bool playerMove = true;
 float shotTimer = 0.0f;
 int shotCount = 0;
 bool closeUp = false;
+int lastZDir = 0;
+float lastRotY = 3.14593f;
+bool posSet = false;
+float newAngle = 0.0f;
+float rotY = 0.0f;
+float currentHeight = 0.15f;
+float x1 = 0.0f;
+
+
 
 
 #define SHOT_TIME_START   0.0f
@@ -29,9 +38,11 @@ bool closeUp = false;
 float get_time_s() {
   return (float)((double)get_ticks_us() / 1000000.0);
 }
- int signum(int num) {
+
+int signum(int num) {
   return (num > 0) - (num < 0);
- };
+};
+
 
 int main()
 {
@@ -41,7 +52,7 @@ int main()
 
   dfs_init(DFS_DEFAULT_LOCATION);
 
-  display_init(RESOLUTION_320x240, DEPTH_16_BPP, 3, GAMMA_NONE, FILTERS_RESAMPLE_ANTIALIAS);
+  display_init(RESOLUTION_320x240, DEPTH_16_BPP, 3, GAMMA_NONE, FILTERS_RESAMPLE);
 
   rdpq_init();
   joypad_init();
@@ -51,27 +62,25 @@ int main()
   T3DViewport viewport = t3d_viewport_create();
 
   T3DMat4FP* modelMatFP = malloc_uncached(sizeof(T3DMat4FP));
-
   T3DMat4FP* mapMatFP = malloc_uncached(sizeof(T3DMat4FP));
-
   T3DMat4FP* cubeMatFP = malloc_uncached(sizeof(T3DMat4FP));
 
   t3d_mat4fp_from_srt_euler(mapMatFP, (float[3]){0.3f, 0.3f, 0.3f}, (float[3]){0, 0, 0}, (float[3]){0, 0, -10});
 
   T3DVec3 camPos = {{0, 45.0f, 90.0f}};
   T3DVec3 camTarget = {{0, 0,-10}};
-
   T3DVec3 lightDirVec = {{1.0f, 1.0f, 1.0f}};
+
   t3d_vec3_norm(&lightDirVec);
 
-  // uint8_t colorAmbient[4] = {0xCC, 0xCC, 0xCC, 0x00};
-  // uint8_t colorDir[4]     = {0xff, 0xff, 0xff, 0xff};
+
   uint8_t colorAmbient[4] = {0xaa, 0xaa, 0xaa, 0xaa};
   uint8_t colorDir[4]     = {0xdd, 0xdd, 0xdd, 0xdd};
 
   // uint8_t colorDir[4]     = {0xFF, 0xAA, 0xAA, 0xFF};
 
-  T3DModel *modelMap = t3d_model_load("rom:/grid2.t3dm");
+  //Grid4 is 15 X 24
+  T3DModel *modelMap = t3d_model_load("rom:/grid4.t3dm");
   T3DModel *modelShadow = t3d_model_load("rom:/shadow2.t3dm");
 
   // Model Credits: Quaternius (CC0) https://quaternius.com/packs/easyenemy.html
@@ -84,8 +93,6 @@ int main()
 
   //correct rotation bad animation
   //T3DModel *model = t3d_model_load("rom:/samis2.t3dm");
-
-
 
   // First instantiate skeletons, they will be used to draw models in a specific pose
   // And serve as the target for animations to modify
@@ -118,14 +125,6 @@ int main()
   T3DAnim animCurrent = animIdle;
 
 
-
-  // multiple animations can attach to the same skeleton, this will NOT perform any blending
-  // rather the last animation that updates "wins", this can be useful if multiple animations touch different bones
-  // T3DAnim animAttack = t3d_anim_create(model, "Snake_Attack");
-  // t3d_anim_set_looping(&animAttack, false); // don't loop this animation
-  // t3d_anim_set_playing(&animAttack, false); // start in a paused state
-  // t3d_anim_attach(&animAttack, &skel);
-
   rspq_block_begin();
     t3d_matrix_push(modelMatFP);
     //rdpq_set_prim_color(RGBA32(255, 255, 255, 255));
@@ -155,13 +154,13 @@ int main()
   rspq_syncpoint_t syncPoint = 0;
 
   T3DVec3 moveDir = {{0,0,0}};
-  T3DVec3 playerPos = {{0,0.15f,0}};
+  T3DVec3 playerPos = {{0,currentHeight,0}};
   T3DVec3 lastPlayerPos = playerPos;
 
   T3DVec3 cubePos = {{0,playerPos.v[1]+26,0}};
+  T3DVec3 shotDir = {{0,0,0}};
 
 
-  float rotY = 0.0f;
   float rotX = -1.50f;
   float rotZ = 0.0f;
   float currSpeed = 0.0f;
@@ -189,14 +188,11 @@ int main()
     }};
     float speed = sqrtf(t3d_vec3_len2(&newDir));
 
-    // Player Attack
-    // if((btn.a || btn.b) && !animAttack.isPlaying) {
-    //   t3d_anim_set_playing(&animAttack, true);
-    //   t3d_anim_set_time(&animAttack, 0.0f);
-    //   isAttack = true;
-    // }
 
-    if(btnHeld.z ) {
+
+
+    // Close up view
+    if(btnHeld.z) {
       closeUp = true;
       playerMove = false;
     } else {
@@ -204,21 +200,26 @@ int main()
       playerMove = true;
     }
 
-
-
-
     // Player movement
     if(speed > 0.15f) {
       newDir.v[0] /= speed;
       newDir.v[2] /= speed;
 
       moveDir = newDir;
+      if (!shotFired) {
+        shotDir = newDir;
+      }
 
+      newAngle = atan2f(moveDir.v[0], moveDir.v[2]);
 
-      // what does this return?
-      float newAngle = atan2f(moveDir.v[0], moveDir.v[2]);
-      //rotY = t3d_lerp_angle(0.0f, newAngle, 0.25f);
-      rotY = t3d_lerp_angle(0.0f, 1.0f, newAngle); //adjusting for swapped y and z axis
+      if (playerMove) {
+        rotY = t3d_lerp_angle(0.0f, 1.0f, newAngle); //adjusting for swapped y and z axis
+      } else {
+        if ( (newAngle <= -2.0f && newAngle >= -3.14593f) || (newAngle >= 2.0f && newAngle <= 3.14593f)) {
+          rotY = t3d_lerp_angle(0.0f, 1.0f, newAngle); //adjusting for swapped y and z axis
+        }
+      }
+
       currSpeed = t3d_lerp(currSpeed, speed * 0.15f, 0.15f);
     } else {
       currSpeed *= 0.8f;
@@ -232,7 +233,7 @@ int main()
       isRun = false;
     }
 
-    if (btn.a && !isJump) {
+    if (btn.a && (!isJump && playerMove)) {
       aPress = true;
       isJump = true;
       t3d_anim_set_playing(&animJump, true);
@@ -241,11 +242,18 @@ int main()
       aPress = false;
     }
 
-     if (btn.b && shotCount < 3 ) {
+     if (btn.b && !shotFired ) {
         shotCount++;
         shotFired = true;
         shotTimer = 0.0f;
+        cubePos.v[1] = playerPos.v[1]+26;
      }
+
+    if (newDir.v[2] > 0.0f || newDir.v[2] < 0.0f) {
+        lastZDir = newDir.v[2];
+      }else {
+        lastZDir = -1;
+      }
 
 
     if (isRun) {
@@ -256,17 +264,46 @@ int main()
     }
 
     if (isJump) {
-      currSpeed = 1.5f;
+      currSpeed = 2.5f;
       //playerPos.v[1] += 0.08f;
+      if (!isRun) {
+        moveDir.v[0] = 0.0f;
+        moveDir.v[1] += 0.08f;
+        moveDir.v[2] = 0.0f;
+      }
     } else {
-      playerPos.v[1] = 0.15f;
+      //playerPos.v[1] = 0.15f;
+      playerPos.v[1] = currentHeight;
     }
 
+
+//hegiht 23
+// distance 126
+// speed = currSpeed
+
+// How do I figure out how long the player is travelling up a slope using the height, distand and speed?
+
+
+    // I can turn this into a function
     //height test
-    if (playerPos.v[0] < 0.0f && playerPos.v[0] > -130.0f) {
-      if (playerPos.v[2] < -10.0f && playerPos.v[2] > -63.0f) {
-        playerPos.v[1] = 18.0f;
+    float distance = 126.00f;
+    if (playerPos.v[0] < -90.0f && playerPos.v[0] > -236.0f) {
+      if (playerPos.v[2] < 145.0f && playerPos.v[2] > -133.0f) {
+        if (x1 != playerPos.v[0]) {
+          x1 = playerPos.v[0];
+          if (currentHeight <= 28.0f) {
+            currentHeight += 0.5f * currSpeed;
+          }
+        }
       }
+    } else {
+      // playerPos.v[1] -= 0.00038f;
+       if (x1 != playerPos.v[0]) {
+          x1 = playerPos.v[0];
+          if (currentHeight >= 0.150f) {
+            currentHeight -= 0.5f * currSpeed;
+          }
+        }
     }
 
 
@@ -279,19 +316,32 @@ int main()
       playerPos.v[0] += moveDir.v[0] * currSpeed;
       playerPos.v[2] += moveDir.v[2] * currSpeed;
       lastPlayerPos = playerPos;
+      posSet = false;
+
     } else {
       playerPos = lastPlayerPos;
+      if (!posSet) {
+        rotY = 3.14593f;
+        posSet = true;
+        shotDir = newDir;
+      }
     }
 
 
-    shotSpeed = 10.0f;
+    shotSpeed = 40.0f;
 
     if (shotFired) {
       shotTimer += deltaTime;
       if (shotTimer > SHOT_TIME_START && shotTimer < SHOT_TIME_END) {
-        cubePos.v[0] += moveDir.v[0] * shotSpeed;
-        cubePos.v[1] = playerPos.v[1]+26;
-        cubePos.v[2] += moveDir.v[2] * shotSpeed;
+        cubePos.v[0] += shotDir.v[0] * shotSpeed;
+        cubePos.v[2] += shotDir.v[2] * shotSpeed;
+        if (!playerMove) {
+          if (cubePos.v[1] <= 50 && cubePos.v[1] >= 0) {
+            //cubePos.v[1] += 0.5f;
+            newDir.v[1] = newDir.v[2]* -1;
+            cubePos.v[1] += (newDir.v[2]* -1) * shotSpeed/5; // need to save the last direction
+          }
+        }
       } else {
         shotCount --;
         shotFired = false;
@@ -305,7 +355,7 @@ int main()
 
     // ...and limit position inside the box
     // const float BOX_SIZE = 140.0f;
-    const float BOX_SIZE = 240.0f;
+    const float BOX_SIZE = 340.0f;
     if(playerPos.v[0] < -BOX_SIZE)playerPos.v[0] = -BOX_SIZE;
     if(playerPos.v[0] >  BOX_SIZE)playerPos.v[0] =  BOX_SIZE;
     if(playerPos.v[2] < -BOX_SIZE)playerPos.v[2] = -BOX_SIZE;
@@ -368,7 +418,7 @@ int main()
 
     // this is where player movement is set
     t3d_mat4fp_from_srt_euler(modelMatFP,
-      (float[3]){0.25f, 0.25f, 0.25f},
+      (float[3]){0.19f, 0.19f, 0.19f},
       //(float[3]){0.006f, 0.006f, 0.006f},
       //(float[3]){0.0f, 0.0f, rotY},
       (float[3]){rotX, 0.0f, rotY}, // rotation - puts the model upright
@@ -421,12 +471,24 @@ int main()
     rdpq_sync_pipe();
     // rdpq_text_printf(NULL, FONT_BUILTIN_DEBUG_MONO, posX, posY, "[A] Attack: %d", isAttack);
 
-    posY = 150;
+    posY = 130;
+    // rdpq_text_printf(NULL, FONT_BUILTIN_DEBUG_MONO, posX, posY, "last rotY %f", lastRotY); posY += 10;
+
+    // rdpq_text_printf(NULL, FONT_BUILTIN_DEBUG_MONO, posX, posY, "rotY %f", rotY); posY += 10;
+    // rdpq_text_printf(NULL, FONT_BUILTIN_DEBUG_MONO, posX, posY, "newangle %f", newAngle); posY += 10;
+    // rdpq_text_printf(NULL, FONT_BUILTIN_DEBUG_MONO, posX, posY, "movDir1 %f", moveDir.v[0]); posY += 10;
+    // rdpq_text_printf(NULL, FONT_BUILTIN_DEBUG_MONO, posX, posY, "movDir2 %f", moveDir.v[2]); posY += 10;
+
+    // rdpq_text_printf(NULL, FONT_BUILTIN_DEBUG_MONO, posX, posY, "x %f", newDir.v[0]); posY += 10;
+    // rdpq_text_printf(NULL, FONT_BUILTIN_DEBUG_MONO, posX, posY, "y %f", newDir.v[1]); posY += 10;
+    // rdpq_text_printf(NULL, FONT_BUILTIN_DEBUG_MONO, posX, posY, "z %f", newDir.v[2]); posY += 10;
+    rdpq_text_printf(NULL, FONT_BUILTIN_DEBUG_MONO, posX, posY, "x1 %f", x1); posY += 10;
+
     rdpq_text_printf(NULL, FONT_BUILTIN_DEBUG_MONO, posX, posY, "x %f", playerPos.v[0]); posY += 10;
     rdpq_text_printf(NULL, FONT_BUILTIN_DEBUG_MONO, posX, posY, "y %f", playerPos.v[1]); posY += 10;
     rdpq_text_printf(NULL, FONT_BUILTIN_DEBUG_MONO, posX, posY, "z %f", playerPos.v[2]); posY += 10;
-    rdpq_text_printf(NULL, FONT_BUILTIN_DEBUG_MONO, posX, posY, "Speeeeed: %.4f", currSpeed); posY += 10;
-    rdpq_text_printf(NULL, FONT_BUILTIN_DEBUG_MONO, posX, posY, "Blend: %.4f", animBlend); posY += 10;
+    // rdpq_text_printf(NULL, FONT_BUILTIN_DEBUG_MONO, posX, posY, "Speeeeed: %.4f", currSpeed); posY += 10;
+    // rdpq_text_printf(NULL, FONT_BUILTIN_DEBUG_MONO, posX, posY, "Blend: %.4f", animBlend); posY += 10;
 
     rdpq_detach_show();
   }
